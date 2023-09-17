12fc6ca2fbd7180438d9952217ab33da53af9a20
Robert <robertgleison12@gmail.com>
2023-09-17 13:38:06 +0100

    Abstracted database connection with other classes

--- courseJDBC/src/main/java/application/Main.java
+++ courseJDBC/src/main/java/application/Main.java
@@ -1,32 +1,12 @@
 package application;
 
+import database.DatabaseConnection;
 
-import java.sql.*;
+import java.sql.Connection;
 
-/* 1- import sql package
-*  2- load and register driver (connector)
-*  3- create connection
-*  4- create statement
-*  5- execute query
-*  6- process result
-*  7- close
-* */
 public class Main {
     public static void main(String[] args) {
-        String url = "jdbc:mysql://localhost:3306/courseJDBC";
-        String user = "root";
-        String password = "";
-        try{
-            Connection connection = DriverManager.getConnection(url,user,password);
-            Statement st = connection.createStatement();
-            ResultSet rs = st.executeQuery("");
-
-
-            st.close();
-            connection.close();
-        }
-        catch(SQLException e){
-            System.out.println("Error: " + e.getMessage());;
-        }
+        Connection connection = DatabaseConnection.getConnection();
+        DatabaseConnection.closeConnection();
     }
 }
---
+++ courseJDBC/src/main/java/database/DatabaseConnection.java
@@ -0,0 +1,48 @@
+package database;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DatabaseConnection {
+
+    private static Connection connection = null;
+
+    public static Connection getConnection() {
+        if (connection == null) {
+            try {
+                Properties props = loadProperties();
+                String url = props.getProperty("url");
+                String user = props.getProperty("user");
+                String password = props.getProperty("password");
+                connection = DriverManager.getConnection(url, user, password);
+            } catch (SQLException e) {
+                throw new RuntimeException(e.getMessage());
+            }
+        }
+        return connection;
+    }
+
+    public static void closeConnection() {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e.getMessage());
+            }
+        }
+    }
+
+    private static Properties loadProperties() {
+        try (FileInputStream fs = new FileInputStream("/home/robert/Projects/databases-training-projects/courseJDBC/src/main/resources/db.properties")) {
+            Properties props = new Properties();
+            props.load(fs);
+            return props;
+        } catch (IOException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+}
